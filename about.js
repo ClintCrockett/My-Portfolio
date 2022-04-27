14dea11e4a542e367d70d252a3ee68d55085d914
Clint88 <ccrockett154@west-mec.org>
2022-04-26 23:43:17 -0600

    Content added to JS files

---
+++ about.js
@@ -0,0 +1,5 @@
+function spinImg(spin){
+    if(spin = "spinImg"){
+        document.getElementsByTagName("img")[0].style.transform = "rotateY(360deg) 6s"
+    }
+}
---
+++ home.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function spinImg(spin){
+    if(spin = "spinImg"){
+        document.getElementById("imgSpin")[0].style.transform = "rotate(360deg)"
+        document.getElementById("imgSpin")[0].style.transition = "1.5s";
+    }
+}
